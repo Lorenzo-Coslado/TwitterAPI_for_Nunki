9d23a51682de1e7677cb8be97143718f4cd3320e
Popipoplo <93212434+Popipoplo@users.noreply.github.com>
2022-09-26 23:49:26 +0200

    Update API_getUser+getTweet(keyword&boolean).py

--- API_getUser+getTweet(keyword&boolean).py
+++ API_getUser+getTweet(keyword&boolean).py
@@ -4,8 +4,9 @@ from flask import Flask
 
 ### Connection à L'API de Twitter ###
 
-auth = tweepy.OAuthHandler("CxCkNuSCmuelWNslaQhm9G7bq" , "gJMNhCM6GoBAeK086LELVSunbdfSo1a02yeR0ehmAKtA8pkUZT")
-auth.set_access_token("1365307419291746313-i2bEAmjz6NPErFs3cVhyE1LG0KF7SO" , "KUCoEdshHZ7VUqKrY7S7FVUrFmjOPaD6uznOoHjzXQhMo")
+### Remplacez les etoiles ###
+auth = tweepy.OAuthHandler("****" , "****")
+auth.set_access_token("****" , "****")
 
 api = tweepy.API(auth)
 
@@ -45,9 +46,6 @@ def getTweet(keyword, boolean):
     return tweet_text
 
 
-
-
-
 ### Route pour mon getUser ###
 
 @app.route("/user=<userId>", methods = ['GET'])
@@ -59,41 +57,6 @@ def getUser(userId):
     user = api.get_user(user_id = userId)
 
     return str(user)
-
-
-
-
-
-    
-### Route & Fonction Bonus pour l'activer enlever les ""### 
-
-'''
-@app.route("/search=<keyword>/media=<boolean>/max=<max_tweets>", methods = ['GET'])
-def getTweet(keyword, boolean):
-
-    keyword_filter = keyword + " -filter:retweets"
-    max_tweets = 10
-    include_entities = boolean
-    final = []
-    i = 0
-
-
-    tweets = tweepy.Cursor(api.search_tweets, q = keyword_filter, tweet_mode = 'extended', include_entities = include_entities).items(max_tweets)
-
-    for tweet in tweets:
-
-        i = i+1
-        tweet_text = str(tweet.full_text)
-        tweetId = tweet.id
-        tweet_link = "https://twitter.com/i/web/status/" + str(tweetId)
-        tweet_time = str(tweet.created_at).replace("+00:00", "")
-        tweet_source = tweet.source
-        tweet_user = str("@" + str(tweet.author.screen_name))
-        final.append("Tweet numero " + str(i) + " :   " + tweet_text + "   " + tweet_link + "   " + tweet_time + "   " + tweet_source + "   " + tweet_user )
-
-
-    return final
-'''
 
 
 if __name__ == '__main__' :
